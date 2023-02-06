27d914535fe063b8dce14055362e6be0648ce0b7
Thomas01010 <124602407+Thomas01010@users.noreply.github.com>
2023-02-06 18:18:26 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+var xhr = new XMLHttpRequest();
+var url = "url";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.email + ", " + json.password);
+    }
+};
+var data = JSON.stringify({"title": "Appli", "icon": "https://components.connect.trimble.com/trimble-connect-workspace-api/examples/logo192.png", "command" : "QUADRI_TOP_MENU"});
+xhr.send(data);
